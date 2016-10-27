d2c96643fb265378552a343bdbb1550ab229dc67
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-27 17:14:51 -0200

    Adicionei as infos do usuario na tela, falta ajeitar

--- projProfParticular/www/features/professores/professorCtrl.js
+++ projProfParticular/www/features/professores/professorCtrl.js
@@ -10,4 +10,69 @@ function ($scope, $stateParams) {
 	console.log("ProfessorCtrl| estou aqui");
 
 
+	professorCtrl.user = {
+		displayName: 'Blaaa',
+		photoURL: 'img/null-avatar.png',
+		email: 'bla@hot'
+	};
+
+	professorCtrl.professor = {
+		"UID": "8B1eYE4JZ8MYTpVjYBZFlhGJBO52",
+		"displayName":"Danilo Aleixo",
+		"photoURL":"https://lh6.googleusercontent.com/-sjP2tqfdCTw/AAAAAAAAAAI/AAAAAAAADF8/pQqKYkuBcE4/photo.jpg",
+		"materias":{
+			"resumo":["matematica", "fisica"],
+			"fundamental":["matematica", "fisica"],
+			"medio":["matematica", "fisica"],
+			"superior":[]
+		},
+		"localizacoes":{
+			"principal": {
+				"logradouro": "Rua xyz, 180",
+				"cidade": "São Paulo ",
+				"estado":"SP",
+				"CEP": "05454555"
+			},
+			"secundararias" : []
+		},
+		"curriculo": {
+			"sobre":"Sou muito legal",
+			"formacao":{
+				"graduacao":{
+					"instituicao": "USP",
+					"status":"Cursando",
+					"curso":"Ciencia da Computacao"
+				},
+				"posGraduacoes":[]
+			}
+		},
+		"avaliacoes":{
+			"quantidade": 0,
+			"didatica":5,
+			"conhecimento":5,
+			"simpatia":5
+		},
+		"horariosDisponiveis":{
+			"segunda":{
+				"8-9":true,
+				"9-10":true,
+				"10-11":false
+			},
+			"terca":{
+
+			}
+		},
+		"agenda":[]
+	};
+
+	// set the rate and max variables
+	professorCtrl.rating = {
+		'max': 5,
+		'readOnly': true
+	}
+
+
+	
+
+
 }])
