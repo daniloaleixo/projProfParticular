26c0f84eb85a8daccab3cc4d735dd3fb343fa0f9
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-11-16 16:20:26 -0200

    tentando fazer o loading service ser testado

--- projProfParticular/tests/unit-tests/Services/KeyboardService.tests.js
+++ projProfParticular/tests/unit-tests/Services/KeyboardService.tests.js
@@ -1,15 +1,24 @@
-xdescribe('KeyboardService', function(){
+describe('KeyboardService', function(){
+
+	var KeyboardService = null;
+
+	beforeEach(module('app.services'));
+
+	beforeEach(inject(function(){
+		var $injector = angular.injector(['app.services']);
+		KeyboardService = $injector.get('KeyboardService');
+	}))
 
     describe('Smoke test - ', function(){
     	it('should have started the service', function(){
-
+    		expect(KeyboardService).not.toBe(null);
     	});
     });
 
 
-    describe('hide function - ', function(){
+    xdescribe('hide function - ', function(){
     	it('should call hide keyboard', function(){
-
+    		KeyboardService.hide();
     	})
     })
 
--- projProfParticular/tests/unit-tests/Services/LoadingService.tests.js
+++ projProfParticular/tests/unit-tests/Services/LoadingService.tests.js
@@ -1,25 +1,50 @@
-xdescribe('LoadingService', function(){
+describe('LoadingService', function(){
+
+    var LoadingService = null;
+    var ionicLoadingMock;
+
+    beforeEach(function(){
+        module(function($provide){
+            $provide.service('$ionicLoading', function(){
+                this.show = jasmine.createSpy('show');
+            });
+        });
+      module('app.services');
+    });
+
+    beforeEach(inject(function($window, modalSvc, sampleSvc){
+        mockWindow=$window;
+        sampleSvcObj=sampleSvc;
+    }));
+
+    // beforeEach(module('app.services'));
+
+    // beforeEach(inject(function(){
+    //     var $injector = angular.injector(['app.services']);
+    //     LoadingService = $injector.get('LoadingService');
+    // }))
+
 
     describe('Smoke test - ', function(){
     	it('should have started the service', function(){
-
+            expect(LoadingService).not.toBe(null);
     	});
     });
 
 
-    describe('showLoadingSpinner function - ', function(){
+    xdescribe('showLoadingSpinner function - ', function(){
     	it('should call the ionicLoading witt spinner icon', function(){
 
     	})
     })
 
-    describe('showLoadingUpdating function - ', function(){
+    xdescribe('showLoadingUpdating function - ', function(){
         it('should call the ionicLoading with <atualizando> text', function(){
 
         })
     })
 
-    describe('hideLoading function - ', function(){
+    xdescribe('hideLoading function - ', function(){
         it('should call the hide loading', function(){
 
         })
