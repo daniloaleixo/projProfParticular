78bdfaa97aa913566abc3d553e7526c686bc3b6d
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-12-12 13:09:44 -0200

    Tentando fazer o nighrmateJS rodar os testes

---
+++ projProfParticular/tests/e2e-tests/example.js
@@ -0,0 +1,23 @@
+var Nightmare = require('nightmare');
+var expect = require('chai').expect; // jshint ignore:line
+var should = require('chai').should; // jshint ignore:line
+
+describe('demo', function() {
+  this.timeout(15000); // Set timeout to 15 seconds, instead of the original 2 seconds
+
+  var url = 'http://google.com';
+
+  describe('Start page', function () {
+          it('should show form when loaded', function (done) {
+              new Nightmare() 
+                  .goto(url)
+                  .evaluate(function() {
+                    return document.querySelectorAll('form').length;
+                  })
+                  .run(function(err, result){
+                    result.should.equal(1);
+                    done();
+                  });
+          });
+      });
+});
