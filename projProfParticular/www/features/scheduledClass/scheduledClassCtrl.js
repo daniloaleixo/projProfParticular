b49ebb88025de6b53d2de30b4822e559e09ee22c
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-02-23 15:41:07 -0300

    Criei um novo card com botoes para aulas que precisam ser confirmadas

--- projProfParticular/www/features/scheduledClass/scheduledClassCtrl.js
+++ projProfParticular/www/features/scheduledClass/scheduledClassCtrl.js
@@ -11,7 +11,85 @@ function ($scope, $rootScope, $stateParams, MyScheduledClassesList, LoadingServi
 	scheduledClassCtrl.myScheduledClasses = [];
 	scheduledClassCtrl.toConfirmClasses = [];
 
+
+	scheduledClassCtrl.myScheduledClasses = [{
+			"status": "Pendente",
+			"UIDRequested":"MqhNsmQB69XOR8uUjGKXtvEoP002",
+			"students":{
+				"MqNhsmQB69XOR8uUjGKXtvEoP002":{
+					"displayName":"Bla",
+					"photoURL":""
+				}
+			},
+			"date":"Mon Sep 7 2017 19:00:00 GMT-0300 (BRT)",
+			"duration":"2",
+			"location":{
+				"address":"Av. Senador Vergueiro - Anchieta, São Bernardo do Campo - State of São Paulo, Brazil",
+				"number":"180",
+				"complement":"142A"
+			},
+			"course":{
+				"2-7":"História"
+			},
+			"description":"Quero aprender sobre história do Brasil"
+		},
+		{
+			"status": "Pendente",
+			"UIDRequested":"MqhNsmQB69XOR8uUjGKXtvEoP002",
+			"students":{
+				"MqNhsmQB69XOR8uUjGKXtvEoP002":{
+					"displayName":"Bla",
+					"photoURL":""
+				}
+			},
+			"date":"Mon Sep 8 2017 19:00:00 GMT-0300 (BRT)",
+			"duration":"2",
+			"location":{
+				"address":"Av. Senador Vergueiro - Anchieta, São Bernardo do Campo - State of São Paulo, Brazil",
+				"number":"180",
+				"complement":"142A"
+			},
+			"course":{
+				"2-7":"História"
+			},
+			"description":"Quero aprender sobre história da America"
+		}];
+	scheduledClassCtrl.toConfirmClasses = [{
+	      "UIDRequested" : "MqhNsmQB69XOR8uUjGKXtvEoP002",
+	      "course" : {
+	        "courseCode" : "Física"
+	      },
+	      "date" : "Sat Apr 01 2017 16:00:00 GMT-0300 (BRT)",
+	      "description" : "dasdsaasdasda",
+	      "duration" : "2",
+	      "location" : {
+	        "address" : "Av. Senador Vergueiro - Anchieta, São Bernardo do Campo - State of São Paulo, Brazil",
+	        "number" : 32213
+	      },
+	      "professor" : {
+	        "UID" : "8B1eYE4JZ8MYTpVjYBZFlhGJBO52",
+	        "displayName" : "Danilo Aleixo",
+	        "photoURL" : "https://lh6.googleusercontent.com/-sjP2tqfdCTw/AAAAAAAAAAI/AAAAAAAADF8/pQqKYkuBcE4/photo.jpg"
+	      },
+	      "status" : "Aguardando confirmação do aluno",
+	      "students" : {
+	        "MqhNsmQB69XOR8uUjGKXtvEoP002" : {
+	          "displayName" : "blaaa1234"
+	        }
+	      }
+	    }];
+
+
 	var scheduledOK = false, toConfirmOK = false;
+
+
+	scheduledClassCtrl.moreInfoClassToConfirm = function(){
+		console.log("cliquei na funcao moreInfoClassToConfirm");
+	}
+
+	scheduledClassCtrl.buttonOne = function(){
+		console.log("cliquei na funcao buttonOne");
+	}
 
 
 	scheduledClassCtrl.init = function(){
@@ -31,7 +109,7 @@ function ($scope, $rootScope, $stateParams, MyScheduledClassesList, LoadingServi
 		});
 	}
 	
-	scheduledClassCtrl.init();
+	//scheduledClassCtrl.init();
 
 
 	var checkIfItsOver = function() {
